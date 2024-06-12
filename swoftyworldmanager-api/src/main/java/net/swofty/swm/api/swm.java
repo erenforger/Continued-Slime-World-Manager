06072f3c7a046f933bc556abaa9c0b122aebf0a1
erenforger <98004331+erenforger@users.noreply.github.com>
2024-06-12 06:15:15 +0530

    Create swm.java

---
+++ swoftyworldmanager-api/src/main/java/net/swofty/swm/api/swm.java
@@ -0,0 +1,15 @@
+package net.swofty.swm.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class swm extends JavaPlugin{
+  @Override
+    public void onEnable()
+  { 
+    System.out.println("plugin enabled")
+  }
+  @Override
+    public void onEnable()
+  {
+  }
+}
