43060eddb2ff0ef6c84820f1a9878a01da801563
erenforger <98004331+erenforger@users.noreply.github.com>
2024-06-12 06:55:46 +0530

    Delete swoftyworldmanager-api/src/main/java/net/swofty/swm/api/world directory

--- swoftyworldmanager-api/src/main/java/net/swofty/swm/api/world/SlimeWorldImporter.java
+++
@@ -1,9 +0,0 @@
-package net.swofty.swm.api.world;
-
-import net.swofty.swm.api.exceptions.InvalidWorldException;
-
-import java.io.File;
-
-public interface SlimeWorldImporter {
-    SlimeWorld readFromDirectory(File worldDir) throws InvalidWorldException;
-}
