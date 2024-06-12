c0be6dd943cc27565dab31b4f41bd57c293cc745
erenforger <98004331+erenforger@users.noreply.github.com>
2024-06-12 07:08:04 +0530

    Delete swoftyworldmanager-plugin directory

--- swoftyworldmanager-plugin/src/main/java/net/swofty/swm/plugin/commands/CommandCooldown.java
+++
@@ -1,9 +0,0 @@
-package net.swofty.swm.plugin.commands;
-
-public interface CommandCooldown {
-    long cooldownSeconds();
-
-    default long getCooldown() {
-        return cooldownSeconds() * 1000;
-    }
-}
